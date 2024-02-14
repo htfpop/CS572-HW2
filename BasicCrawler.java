7697cb031886f21a2ddafd04948592ff58ee1f0d
Christopher Leung <ckleung@usc.edu>
2024-02-14 11:41:58 -0800

    Updated parser per requirements

--- BasicCrawler.java
+++ BasicCrawler.java
@@ -2,28 +2,39 @@ import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
 import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
+
+import java.io.*;
 import java.util.regex.Pattern;
 import java.util.Set;
 import org.apache.http.Header;
 
+import java.util.concurrent.atomic.AtomicInteger;
 
 // -need- to implement visit() and shouldVisit()
 public class BasicCrawler extends WebCrawler {
 
-    private static final Pattern IMAGE_EXTENSIONS = Pattern.compile(".*\\.(bmp|gif|jpg|png)$");
+    private static final Pattern IMAGE_EXTENSIONS = Pattern.compile(".*\\.(bmp|gif|jpg|png|CSS|EXE|GIF|JPEG|MP3|PDF)$");
+    CrawlStat myCrawlStat;
 
-    //private final AtomicInteger numSeenImages;
+    //private final AtomicInteger numPagesSeen;
+
+
+    public BasicCrawler() {
+        //this.numPagesSeen = 0;
+        myCrawlStat = new CrawlStat();
+    }
 
     /**
      * Creates a new crawler instance.
      *
-     * @param numSeenImages This is just an example to demonstrate how you can pass objects to crawlers. In this
+     * @param numPagesSeen This is just an example to demonstrate how you can pass objects to crawlers. In this
      * example, we pass an AtomicInteger to all crawlers and they increment it whenever they see a url which points
      * to an image.
      */
-    public BasicCrawler() {
-        //this.numSeenImages = 10;
+    public BasicCrawler(AtomicInteger numPagesSeen) {
+         //this.numPagesSeen= numPagesSeen;
     }
+
 
     /**
      * You should implement this function to specify whether the given url
@@ -48,6 +59,9 @@ public class BasicCrawler extends WebCrawler {
      */
     @Override
     public void visit(Page page) {
+        logger.info("Visited: {}", page.getWebURL().getURL());
+        myCrawlStat.incProcessedPages();
+
         int docid = page.getWebURL().getDocid();
         String url = page.getWebURL().getURL();
         String domain = page.getWebURL().getDomain();
@@ -55,6 +69,7 @@ public class BasicCrawler extends WebCrawler {
         String subDomain = page.getWebURL().getSubDomain();
         String parentUrl = page.getWebURL().getParentUrl();
         String anchor = page.getWebURL().getAnchor();
+        int statusCode = page.getStatusCode();
 
         System.out.println("Docid: " + docid);
         System.out.println("URL: " + url);
@@ -63,6 +78,7 @@ public class BasicCrawler extends WebCrawler {
         System.out.println("Path: "+ path);
         System.out.println("Parent page: " + parentUrl);
         System.out.println("Anchor text: " +  anchor);
+        System.out.println("Status " +  statusCode);
 
         if (page.getParseData() instanceof HtmlParseData) {
             HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
@@ -70,9 +86,25 @@ public class BasicCrawler extends WebCrawler {
             String html = htmlParseData.getHtml();
             Set<WebURL> links = htmlParseData.getOutgoingUrls();
 
+            myCrawlStat.incTotalLinks(links.size());
+            try {
+                myCrawlStat.incTotalTextSize(htmlParseData.getText().getBytes("UTF-8").length);
+            } catch (UnsupportedEncodingException ignored) {
+                // Do nothing
+            }
+
             logger.debug("Text length: {}", text.length());
             logger.debug("Html length: {}", html.length());
             logger.debug("Number of outgoing links: {}", links.size());
+
+            // Append to log
+            String add = url +';'+ statusCode;
+            myCrawlStat.stackPush(add);
+
+            // We dump this crawler statistics after processing every 50 pages
+            if ((myCrawlStat.getTotalProcessedPages() % 50) == 0) {
+                dumpMyData();
+            }
         }
 
         Header[] responseHeaders = page.getFetchResponseHeaders();
@@ -86,4 +118,58 @@ public class BasicCrawler extends WebCrawler {
 
         System.out.println("\n==========================\n");
     }
+
+    public void dumpMyData() {
+        int id = getMyId();
+        // You can configure the log to output to file
+        logger.info("Crawler {} > Processed Pages: {}", id, myCrawlStat.getTotalProcessedPages());
+        logger.info("Crawler {} > Total Links Found: {}", id, myCrawlStat.getTotalLinks());
+        logger.info("Crawler {} > Total Text Size: {}", id, myCrawlStat.getTotalTextSize());
+    }
+
+    /**
+     * This function is called by controller to get the local data of this crawler when job is
+     * finished
+     */
+    @Override
+    public Object getMyLocalData() {
+        return myCrawlStat;
+    }
+
+    /**
+     * This function is called by controller before finishing the job.
+     * You can put whatever stuff you need here.
+     */
+    @Override
+    public void onBeforeExit() {
+        dumpMyData();
+        try {
+            write2File();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void write2File() throws IOException {
+        String file = "Thread"+getMyId()+"_log.txt";
+
+        try {
+            PrintWriter pw = new PrintWriter(new FileWriter("logs\\" + file));
+            int stackSize = myCrawlStat.stackSize();
+            for(int i = 0; i < stackSize ; i++)
+            {
+                pw.println(myCrawlStat.stackPop());
+            }
+            pw.flush();
+            pw.close();
+
+        } catch (IOException e)
+        {
+            System.out.printf("An error occurred in thread %d",getMyId());
+            System.exit(-1000);
+        }
+    }
+
+
 }// BasicCrawler
+
--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
+import ch.qos.logback.classic.Logger;
 import edu.uci.ics.crawler4j.crawler.CrawlConfig;
 import edu.uci.ics.crawler4j.crawler.CrawlController;
 import edu.uci.ics.crawler4j.fetcher.PageFetcher;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
+
+import java.util.List;
 
 public class Main {
 
@@ -17,7 +20,7 @@ public class Main {
 
         // Be polite: Make sure that we don't send more than 1 request per second (1000 milliseconds between requests).
         // Otherwise it may overload the target servers.
-        config.setPolitenessDelay(1000);
+        config.setPolitenessDelay(800);
 
         // You can set the maximum crawl depth here.
         // CSCI-572: maximum depth should be set to 16 to ensure that we limit the crawling
@@ -25,7 +28,7 @@ public class Main {
 
         // You can set the maximum number of pages to crawl. The default value is -1 for unlimited number of pages.
         // CSCI-572: should be set to 20,000 to ensure a reasonable execution time for this exercise
-        config.setMaxPagesToFetch(20000);
+        config.setMaxPagesToFetch(1000);
 
         // Should binary data should also be crawled? example: the contents of pdf, or the metadata of images etc
         config.setIncludeBinaryContentInCrawling(false);
@@ -62,16 +65,43 @@ public class Main {
         // Number of threads to use during crawling. Increasing this typically makes crawling faster. But crawling
         // speed depends on many other factors as well. You can experiment with this to figure out what number of
         // threads works best for you.
-        int numberOfCrawlers = 10;
+        int numberOfCrawlers = 16;
 
         // To demonstrate an example of how you can pass objects to crawlers, we use an AtomicInteger that crawlers
         // increment whenever they see a url which points to an image.
-        //AtomicInteger numSeenImages = new AtomicInteger();
+        //AtomicInteger numPagesSeen = new AtomicInteger();
+
+        // The factory which creates instances of crawlers.
+        //CrawlController.WebCrawlerFactory<BasicCrawler> factory = () -> new BasicCrawler(numPagesSeen);
 
         // Start the crawl. This is a blocking operation, meaning that your code
         // will reach the line after this only when crawling is finished.
         // GO!!!!
+        long elapsed = System.nanoTime();
         controller.start(BasicCrawler.class, numberOfCrawlers);
+        elapsed = System.nanoTime() - elapsed;
+
+        List<Object> crawlersLocalData = controller.getCrawlersLocalData();
+        long totalLinks = 0;
+        long totalTextSize = 0;
+        int totalProcessedPages = 0;
+        for (Object localData : crawlersLocalData) {
+            CrawlStat stat = (CrawlStat) localData;
+            totalLinks += stat.getTotalLinks();
+            totalTextSize += stat.getTotalTextSize();
+            totalProcessedPages += stat.getTotalProcessedPages();
+        }
+
+        long sec = elapsed/1000000000;
+        long min = sec/60;
+        sec = sec - min*60;
+
+        System.out.print("Aggregated Statistics:\n");
+        System.out.printf("\tProcessed Pages: {%d}\r\n", totalProcessedPages);
+        System.out.printf("\tTotal Links found: {%d}\r\n", totalLinks);
+        System.out.printf("\tTotal Text Size: {%d}\n", totalTextSize);
+        System.out.printf("\tTotal Time %d min %d s\n", min,sec);
+        System.out.printf("\tTotal s %d\n", elapsed);
 
     }
 
